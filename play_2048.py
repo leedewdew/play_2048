c5eb1ce3de72be2750876c805c4d497958e205c1
lilulu7 <lilulu7@hikvision.com>
2017-07-07 17:57:09 +0800

    add grid_Rand_Fill interface

--- play_2048.py
+++ play_2048.py
@@ -10,7 +10,6 @@ grid_core = "list_version" # "numpy_version"
 grid_board = [lst for lst in [range(4)] for i in range(4)] if grid_core == "list_version" \
     else np.zeros((4,4))
 state = 'start'
-# state = 'update'
 
 def disply_Grid():
     global grid_board
@@ -20,30 +19,11 @@ def disply_Grid():
                 grid_board[row][1],grid_board[row][2],grid_board[row][3])
     print "+---+---+---+---+"
 
-def update_Grid(choice,key=None):
+def grid_Rand_Fill(choice,key=None):
     if choice == 'start':
         print "start:", key
     elif choice == 'update':
         print "update:", key
-
-def main(event):
-    global enter_key, state
-    enter_key = event.Key
-    print "enter_key:", enter_key
-    if state == 'start':
-        update_Grid('start')
-    else:
-        update_Grid('update',enter_key)
-    return True
-
-def start_det_keyboard():
-    hm = pyHook.HookManager()
-    # 监听所有键盘事件
-    hm.KeyDown = main
-    # 设置键盘“钩子”
-    hm.HookKeyboard()
-    # 进入循环，如不手动关闭，程序将一直处于监听状态
-    pythoncom.PumpMessages()
 
 def merge_Grid():
     pass
@@ -51,7 +31,29 @@ def merge_Grid():
 def is_Over():
     pass
 
+def main(event):
+    global enter_key, state
+    enter_key = event.Key
+    print "enter_key:", enter_key
+    if state == 'start':
+        grid_Rand_Fill('start')
+        state = 'update'
+    else:
+        merge_Grid()
+        grid_Rand_Fill('update',enter_key)
+
+    disply_Grid()
+    if is_Over():
+        print "Over!"
+    return True
+
+def start_det_keyboard():
+    hm = pyHook.HookManager()
+    hm.KeyDown = main
+    hm.HookKeyboard()
+
+    pythoncom.PumpMessages()
+
 if __name__ == "__main__":
     target_num = 32
-    # start_det_keyboard()
-    disply_Grid()
+    start_det_keyboard()
