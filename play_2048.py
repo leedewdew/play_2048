1582070375a28c2c5660bc743548df806c4be886
lilulu <07122096@163.com>
2017-07-19 22:00:19 +0800

    change shiftAndMerge to shiftRow

--- play_2048.py
+++ play_2048.py
@@ -17,7 +17,7 @@ def row_Display_Format(value):
     else:
         return value
 
-def disply_Grid():
+def display_Grid():
     global grid_board
     for row in range(4):
         row_grid_board = map(str,map(row_Display_Format,grid_board[row]))
@@ -41,14 +41,16 @@ def grid_Rand_Fill(choice,key=None):
     col_to_fill = block_to_fill % 4
     grid_board[row_to_fill][col_to_fill] = random.choice([2,4])
 
-def shiftAndMerge(lst,direction):
+def shiftRow(lst,direction):
     # get non-zero element
-    shift_row_list = [filter(lambda x: x != 0, row) for row in lst]
+    shift_row_list = filter(lambda x: x != 0, lst)
     # fill zero to len=4
-    for i, row in enumerate(shift_row_list):
-        while len(shift_row_list[i]) < 4:
-            shift_row_list[i].append(0)
+    while len(shift_row_list) < 4:
+        shift_row_list.append(0)
     return  shift_row_list
+
+def mergeRow(lst,direction):
+    pass
 
 def merge_Grid(enter_key):
     global grid_board
@@ -66,30 +68,48 @@ def merge_Grid(enter_key):
                 pass
     elif enter_key == 'A': # left
         # shift and merge, then update grid_board
-        grid_board = shiftAndMerge(grid_board, 'A')
+        for row in range(4):
+            grid_board[row] = shiftRow(grid_board[row], 'A')
+        # display_Grid() # debug
+        # print #debug
         for row in range(4):
             for col in range(4):
                 if col <= 2:
-                    if  (not grid_board[row][col]) and (grid_board[row][col] == grid_board[row][col+1]):
+                    if (grid_board[row][col]) and (grid_board[row][col] == grid_board[row][col+1]):
                         grid_board[row][col] *= 2
-                        grid_board[row][col + 1] = 0 # remove element in case merging with next one
-                    merge_row_list.append(grid_board[row][col])
-            grid_board[row] = shiftAndMerge(merge_row_list)
+                        # remove element in case merging with next one
+                        grid_board[row][col+1] = 0
+
+                merge_row_list.append(grid_board[row][col])
+
+            # merge row, then assign to grid_board row
+            grid_board[row] = shiftRow(merge_row_list,'A')
+            # display_Grid()  # debug
             merge_row_list = []
-                else: # col+1 out of index
-                    merge_row_list.append(grid_board[row][col])
     elif enter_key == 'D': # right
-        # get non-zero element
-        shift_row_list = [filter(lambda x: x != 0, row) for row in grid_board]
-        # fill zero to len=4
-        for i, row in enumerate(shift_row_list):
-            while len(shift_row_list[i]) < 4:
-                shift_row_list[i].insert(0,0)
-        # update grid_board
-        grid_board = shift_row_list
+        [grid_board[row].reverse() for row in range(4)]
+        display_Grid()  # debug
+        # shift and merge, then update grid_board
+        for row in range(4):
+            grid_board[row] = shiftRow(grid_board[row], 'A')
+        # display_Grid() # debug
+        print #debug
         for row in range(4):
             for col in range(4):
-                pass
+                if col <= 2 and (grid_board[row][col]) and (grid_board[row][col] == grid_board[row][col+1]):
+                    grid_board[row][col] *= 2
+                    # remove element in case merging with next one
+                    grid_board[row][col+1] = 0
+
+                merge_row_list.append(grid_board[row][col])
+
+            # merge row, then assign to grid_board row
+            grid_board[row] = shiftRow(merge_row_list,'A')
+            merge_row_list = []
+
+        [grid_board[row].reverse() for row in range(4)]
+        display_Grid()  # debug
+        print # debug
 
 def is_Over():
     pass
@@ -105,7 +125,7 @@ def main(event):
         merge_Grid(enter_key)
         grid_Rand_Fill('update')
 
-    disply_Grid()
+    display_Grid()
     if is_Over():
         print "Over!"
     return True
